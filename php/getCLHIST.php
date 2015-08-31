634a0bb0fc18fbb61d9f75092643f9cf1ac14006
Stephen McCready <stephenmccready@gmail.com>
2015-08-31 18:46:36 -0400

    Update getCLHIST.php
    Added last 32 and last 24 options

--- php/getCLHIST.php
+++ php/getCLHIST.php
@@ -4,6 +4,8 @@ ini_set('auto_detect_line_endings',TRUE);
 $filepath="..\data\CLHIST.csv";
 $startYYYY=$_GET["startYYYY"];
 $endYYYY=$_GET["endYYYY"];
+$last32=$_GET["last32"];
+$last24=$_GET["last24"];
 $last16=$_GET["last16"];
 $qtrfin=$_GET["qtrfin"];
 $semifn=$_GET["semifn"];
@@ -23,7 +25,11 @@ if (($handle = fopen($filepath, "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",", "\"")) !== FALSE) {
 
         if($data[0]>=$startYYYY and $data[0]<=$endYYYY
-           and (     (   $data[2]=="5. Last 16" && $last16=="true")
+           and (     (   $data[2]=="7. First Group Stage" && $last32=="true")   // 2000-2007
+                or   (   $data[2]=="7. Group Stage" && $last32=="true")         // 2007-
+                or   (   $data[2]=="8. Group Stage" && $last24=="true")         // Pre 2000
+                or   (   $data[2]=="6. Second Group Stage" && $last16=="true")  // 2000-2007
+                or   (   $data[2]=="5. Last 16" && $last16=="true")             // 2007 -
                 or   (   $data[2]=="4. Quarterfinalist" && $qtrfin=="true")
                 or   (   $data[2]=="3. Semifinalist" && $semifn=="true")
                 or   (   $data[2]=="2. Runner Up" && $runner=="true")
@@ -38,11 +44,15 @@ if (($handle = fopen($filepath, "r")) !== FALSE) {
                 $key = array_search(urlencode($data[$i]), $teams);
                 if($weighted=="true"){
                     switch($data[2]) {
-                        case "5. Last 16": $points=1; break;
-                        case "4. Quarterfinalist": $points=2; break;
-                        case "3. Semifinalist": $points=4; break;
-                        case "2. Runner Up": $points=8; break;
-                        case "1. Winner": $points=16; break;
+                        case "7. First Group Stage": $points=1; break;  // 2000-2007
+                        case "7. Group Stage": $points=1; break;        // 2007-
+                        case "8. Group Stage": $points=2; break;        // Pre 2000
+                        case "6. Second Group Stage": $points=4; break; // 2000-2007
+                        case "5. Last 16": $points=4; break;            // 2007 -
+                        case "4. Quarterfinalist": $points=8; break;
+                        case "3. Semifinalist": $points=16; break;
+                        case "2. Runner Up": $points=32; break;
+                        case "1. Winner": $points=64; break;
                     }
                     
                 } else {
@@ -60,5 +70,4 @@ if (($handle = fopen($filepath, "r")) !== FALSE) {
 }
 
 // teams array contains array of team name, number of appearances
-
 echo json_encode($teams);
