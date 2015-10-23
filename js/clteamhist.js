e304325851e4fc2167dc3e3cb51eec53e2005f3c
Stephen McCready <stephenmccready@gmail.com>
2015-10-23 11:43:35 -0400

    Create clteamhist.js

---
+++ js/clteamhist.js
@@ -0,0 +1,45 @@
+<?php
+ini_set('auto_detect_line_endings',TRUE);
+
+$filepath="..\data\CLHIST.csv";
+$team = $_GET["team"];
+$teamArray = array();
+/* .csv layout is
+    0 Year
+    1 Team
+    2 Stage of Competition
+    3 Country of the teams domestic league
+*/
+$i=1997;
+$toYear=date("Y")+1;
+for ($i; $i<=$toYear; $i++) {
+    $newdata = array (
+                    'year' => strval($i),
+                    'team' => $_GET["team"],
+                    'stage' => strval(0),
+                    'stagetext' => "-",
+                    'country' => ""
+                );
+    $teamArray[$i] = $newdata;
+}
+
+if (($handle = fopen($filepath, "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",", "\"")) !== FALSE) {
+        if(strtoupper($team)==strtoupper($data[1])) {
+            foreach($teamArray as &$value) {
+                if($value['year'] == $data[0]) {
+                   if(substr($data[2],0,1) < $value['stage'] || $value['stage'] == strval(0)) {
+                        $value['team'] = $data[1];
+                        $value['stage'] = substr($data[2],0,1);
+                        $value['stagetext'] = $data[2];
+                        $value['country'] = $data[3];
+                    }
+                    break; // Stop the loop after we've found the item
+                }
+            }
+        }
+    }
+    fclose($handle);
+}
+
+echo json_encode($teamArray);
